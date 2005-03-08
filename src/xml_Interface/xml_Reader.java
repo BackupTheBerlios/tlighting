c280c1852d7b51be66f46f77bc7a3258863cedce
gsilvers <gsilvers>
2005-03-08 03:23:48 +0000

    no message

--- src/xml_Interface/xml_Reader.java
+++ src/xml_Interface/xml_Reader.java
@@ -18,6 +18,12 @@ import Data_Storage.project;
 public class xml_Reader {
     
     public project proj_class; 
+    //Parser Items
+    IXMLParser parser;
+    IXMLReader reader;
+    IXMLElement xml;
+    //Final Holders For Locations
+    public String s_bPath, s_pPath;
     /** Creates a new instance of xml_Reader */
     public xml_Reader() {
         project proj_class=(project)project.oClass;
@@ -26,22 +32,35 @@ public class xml_Reader {
     //main run to load
     //in strings are the path of the project folder as well as the bar file.
     public void load_project(String inProjectPath,String inBarPath) {
+        s_bPath = inBarPath;
+        s_pPath = inProjectPath;
+        get_house();
+        get_bars();
+        get_stage();
+        get_inventory();
+        get_knowntypes();
+        get_instrument();
     }
    
     //individual load functions
-    public void get_house() {
+    private void get_house() {
+        IXMLParser parser = XMLParserFactory.createDefaultXMLParser();
+        IXMLReader reader = StdXMLReader.fileReader("test.xml");
+        parser.setReader(reader);
+        IXMLElement xml = (IXMLElement) parser.parse();
+
     }
-    public void get_bars() {
+    private void get_bars() {
     }
-    public void get_set() {
+    private void get_set() {
     }
-    public void get_stage() {
+    private void get_stage() {
     }
-    public void get_inventory() {
+    private void get_inventory() {
     }
-    public void get_knowntypes() {
+    private void get_knowntypes() {
     }
-    public void get_instrument() {
+    private void get_instrument() {
     }
     
     
