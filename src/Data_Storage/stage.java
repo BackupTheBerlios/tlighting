f2b568351ed7f9b980f2f2747c2b5c9444f4201a
joshzaw <joshzaw>
2005-03-18 09:44:04 +0000

    moved the min max xy fuinctions to general object and made the values global mapp corrdinates

--- src/Data_Storage/stage.java
+++ src/Data_Storage/stage.java
@@ -108,44 +108,6 @@ public class stage extends General_Object{
         return this.linktohouse;
     }
 
-    
-    public int getminx(){
-         int temp=1000;
-        for(int i=0; i<this.vertices; i++){
-            if(this.x[i]<temp){
-                temp=this.x[i];
-            }
-        }
-        return temp;
-    }
-    public int getminy(){
-        int temp=1000;
-        for(int i=0; i<this.vertices; i++){
-            if(this.y[i]<temp){
-                temp=this.y[i];
-            }
-        }
-        return temp;
-    }
-    public int getmaxx(){
-        int temp=0;
-        for(int i=0; i<this.vertices; i++){
-            if(this.x[i]>temp){
-                temp=this.x[i];
-            }
-        }
-        return temp;
-    }
-    public int getmaxy(){
-        int temp=0;
-        for(int i=0; i<this.vertices; i++){
-            if(this.y[i]>temp){
-                temp=this.y[i];
-            }
-        }
-        return temp;
-    }
-
     public void copyStage(stage astage){
         copy_General_Object(astage);
         height=astage.getheight();
