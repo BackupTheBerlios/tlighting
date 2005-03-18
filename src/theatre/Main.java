fa0c2de39d4f94787bde185ef868d8648229d50d
joshzaw <joshzaw>
2005-03-18 09:47:25 +0000

    tightened up the spacing of whitespace so it is more readable

--- src/theatre/Main.java
+++ src/theatre/Main.java
@@ -3,38 +3,22 @@ package theatre;
 
 
 public class Main 
-
 {
-
     public Main()
-
     {
-
-
 
     }
 
     public static void main(String[] args)
-
     {
-
       javax.swing.SwingUtilities.invokeLater(new Runnable() 
-
         { 
-
             public void run() 
-
             {
-
                     BasicWindow bw = new BasicWindow(); 
-
                     bw.createAndShowGUI(); 
-
             } 
-
         }); 
-
     }
-
 }
 
