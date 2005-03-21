3bcd72a92f2122d5575375df8d5de3f89fb09d83
joshzaw <joshzaw>
2005-03-21 11:12:43 +0000

    fixed spacing issues to make code more readable

--- src/theatre/ExplorerBrowserPanel.java
+++ src/theatre/ExplorerBrowserPanel.java
@@ -1,39 +1,78 @@
-package theatre;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import theatre.*;
-import Data_Storage.*;
-
-public class ExplorerBrowserPanel extends Window
-{
-	public static int iWidth = 0;
-	public static int iHeight = 0;
-    public ExplorerBrowserPanel (project proj)
-    {
-        getContentPane().setLayout(null);
-        
-//        JPanel glass=new JPanel();
-/*       glass.addMouseListener(new MouseAdapter() 
-        {
-                public void mousePressed(MouseEvent me)
-                {
-                }
-        } );*/
-//        glass.setOpaque(false);
-        this.setTitle("Stage Items");
-        this.setBackground(Color.WHITE);
-   
-        ExplorerBrowser ib = new ExplorerBrowser(proj);
-        ib.loadStructureTopPoint();
-        getContentPane().add(ib);
-        
-        // set the size based on the system resolution
-        iWidth = BasicWindow.iScreenWidth/5;
-        iHeight = BasicWindow.iScreenHeight;
-        this.setBounds(0, 0, iWidth, iHeight);
-        
-        this.setVisible(true);
-    }
-}
+package theatre;
+
+
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import java.awt.event.*;
+
+import theatre.*;
+
+import Data_Storage.*;
+
+
+
+public class ExplorerBrowserPanel extends Window
+        
+{
+    
+    public static int iWidth = 0;
+    
+    public static int iHeight = 0;
+    
+    public ExplorerBrowserPanel()
+    
+    {
+        
+        getContentPane().setLayout(null);
+        
+        
+        
+        //        JPanel glass=new JPanel();
+        
+/*       glass.addMouseListener(new MouseAdapter()
+ 
+        {
+ 
+                public void mousePressed(MouseEvent me)
+ 
+                {
+ 
+                }
+ 
+        } );*/
+        
+        //        glass.setOpaque(false);
+        
+        this.setTitle("Stage Items");
+        
+        this.setBackground(Color.WHITE);
+        
+        
+        
+        ExplorerBrowser ib = new ExplorerBrowser();
+        
+        ib.loadStructureTopPoint();
+        
+        getContentPane().add(ib);
+        
+        
+        
+        // set the size based on the system resolution
+        
+        iWidth = BasicWindow.iScreenWidth/5;
+        
+        iHeight = BasicWindow.iScreenHeight;
+        
+        this.setBounds(0, 0, iWidth, iHeight);
+        
+        
+        
+        this.setVisible(true);
+        
+    }
+    
+}
+
