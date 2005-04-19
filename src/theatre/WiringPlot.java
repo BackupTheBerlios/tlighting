948ceadf679245bb1cdb01ae50bc835430a3ddfa
gsilvers <gsilvers>
2005-04-19 15:43:11 +0000

    some changes still working

--- src/theatre/WiringPlot.java
+++ src/theatre/WiringPlot.java
@@ -17,12 +17,13 @@ import java.io.*;
  * @author Greg Silverstein
  * 4/14/2005
  */
-public class WiringPlot extends JDialog{
+public class WiringPlot extends JDialog implements ActionListener{
     //UI Objects
     public project proj_class;
     private javax.swing.JTable outTable;
     protected JPanel jp;
-    protected JRadioButton barB, dimB, lightB;
+    protected JButton barB, dimB, lightB;
+    protected JButton btn_print;
     
     //Data
     protected int iScreenHeight =BasicWindow.iScreenWidth-50;
@@ -34,7 +35,7 @@ public class WiringPlot extends JDialog{
         super(BasicWindow.curWindow,"Inventory Manager", true);
         proj_class=(project)project.oClass;
         this.setTitle("Wiring Output Diagram");
-        this.setBounds(20, 20, iScreenHeight/2,iScreenWidth);
+        this.setBounds(20, 20, 600,700);
         initAll();
         this.setVisible(true);
         
@@ -44,16 +45,42 @@ public class WiringPlot extends JDialog{
         jp = new JPanel();
         jp.setLayout(null);
         
-        makeTable();
-        jp.add(outTable);
         
-        barB = new JRadioButton("Sort On Bar");
-        barB.setBounds(((iScreenHeight/2)-450), (iScreenWidth-400), ((iScreenHeight/2)-375),(iScreenWidth-360));
+        barB = new JButton("Sort On Bar");
+        barB.setBounds(30,500,135,30);
         barB.setVisible(true);
+        barB.addActionListener(this);
+        barB.setActionCommand("bar");
         jp.add(barB);
         
+        dimB = new JButton("Sort On Dimmer");
+        dimB.setBounds(30,550,135,30);
+        dimB.setVisible(true);
+        dimB.addActionListener(this);
+        dimB.setActionCommand("dimmer");
+        jp.add(dimB);
+        
+        lightB = new JButton("Sort On Light");
+        lightB.setBounds(30,600,135,30);
+        lightB.setVisible(true);
+        lightB.addActionListener(this);
+        lightB.setActionCommand("light");
+        jp.add(lightB);
+        
+        btn_print = new JButton("Print");
+        btn_print.setBounds(195,500,135,30);
+        btn_print.setVisible(true);
+        btn_print.addActionListener(this);
+        btn_print.setActionCommand("print");
+        
+        jp.add(btn_print);
+        
         System.out.println("Test 1");
-        jp.setBounds(22,22, (iScreenHeight/2)-20,iScreenWidth-20);
+        jp.setBounds(22,22, 780,695);
+        
+        //add table default draw on sort by light
+        selection = 2;
+        makeTable();
         
         getContentPane().add(jp);
     }
@@ -66,8 +93,9 @@ public class WiringPlot extends JDialog{
         Object[][]  data    =  populateTable();
         outTable = new javax.swing.JTable(data, columnNames);
         
-        outTable.setBounds(30,30,(iScreenHeight/2)-105,iScreenWidth-300);
+        outTable.setBounds(30,30,500,465);
         outTable.setVisible(true);
+        jp.add(outTable);
     }
     private Object[][] populateTable() {
         int x = 1;
@@ -75,8 +103,11 @@ public class WiringPlot extends JDialog{
         Object[][] retval;
         
         //TEMP
+        String[] a = new String[5];
+        a = new String[] {"bar", "a", "b","c","d"};
+        
         retval = new Object[][] {
-                    {"a",",b","c"}
+                    {a,a,a}
         };
         if(selection == 1){
             //handle sort on dimmer
@@ -95,5 +126,23 @@ public class WiringPlot extends JDialog{
             //This section handles extra input situation
         }
         return retval;
+    }
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand() == "bar"){
+            selection = 3;
+            makeTable();
+        }
+        else if(e.getActionCommand() == "dimmer"){
+            selection = 1;
+            makeTable();
+        }
+        else if(e.getActionCommand() == "light"){
+            selection = 2;
+            makeTable();
+        }
+        else if(e.getActionCommand() == "print"){
+            PrintUtilities.printComponent(outTable);
+        }
+        
     }
 }
