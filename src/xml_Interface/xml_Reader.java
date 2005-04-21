aa9a362b51a000803f9c1b324f6a7c65e37bbdd1
joshzaw <joshzaw>
2005-04-21 05:28:45 +0000

    added so you can load whole projects at a time or indivual parts

--- src/xml_Interface/xml_Reader.java
+++ src/xml_Interface/xml_Reader.java
@@ -10,7 +10,7 @@ package xml_Interface;
 /**
  *
  * @author Greg Silverstein
- *edited by josh Z 3/21/05
+ *edited by josh Z 3/21/05 nad on 4/20/05
  */
 
 import net.n3.nanoxml.*;
@@ -53,26 +53,28 @@ public class xml_Reader {
         }
         boolean result=false;
         //if it was a house object
-        if(xml.getName().equalsIgnoreCase("house_info")){
+        if(xml.getName().equalsIgnoreCase("project")){
+            result=get_project(xml,inProjectPath);
+        }else if(xml.getName().equalsIgnoreCase("house_info")){
             result=get_house(xml);
         }else if(xml.getName().equalsIgnoreCase("bar_list")){
             //if it was a bar list
             get_bars(xml);
         }else if(xml.getName().equalsIgnoreCase("stage_info")){
             //if it was a stage object
-            get_stage();
+            get_stage(xml);
         }else if(xml.getName().equalsIgnoreCase("set_list")){
             //if it was a set list
-            get_set();
+            get_set(xml);
         }else if(xml.getName().equalsIgnoreCase("inventory_list")){
             //if it was an inventory list
-            get_inventory();
+            get_inventory(xml);
         }else if(xml.getName().equalsIgnoreCase("type_list")){
             //if it was a type list
-            get_knowntypes();
+            get_knowntypes(xml);
         }else if(xml.getName().equalsIgnoreCase("instrument_list")){
             //if it was an instrument list
-            get_instrument();
+            get_instrument(xml);
         }
         return result;
     }
@@ -211,62 +213,312 @@ public class xml_Reader {
             proj_class.addBar(temp_b);
         }
     }
-    private void get_set() {
+    private boolean get_set(IXMLElement xml) {
         
         //create a temporary set list
-        
+        int num=xml.getChildrenCount();
         
         //check if all the items in the list are valid
-        
-        
-        //if they are valid add them to the set list
-        
-        
-        //if they are not valid do not add them
+        int i;
+        for(i=0;i<num;i++){
+            
+            IXMLElement s_xml=xml.getChildAtIndex(i);
+            setobject temp_s=new setobject();
+            //attributes for sets name, description, id, house_id, x, y, z,
+            
+            temp_s.setname(s_xml.getAttribute("name","default"));
+            temp_s.setdescription(s_xml.getAttribute("description","default"));
+            //s_xml.getAttribute("id","0");
+            //s_xml.getAttribute("house_id",((house)proj_class.houses.get_object(0)).getid());
+            temp_s.worldx=Integer.parseInt(s_xml.getAttribute("x","0"));
+            temp_s.worldy=Integer.parseInt(s_xml.getAttribute("y","0"));
+            temp_s.setsize(Integer.parseInt(s_xml.getAttribute("z","0")));
+            
+            //get each node for the set object
+            int i2;
+            int num2=s_xml.getChildrenCount();
+            for(i2=0;i2<num2;i2++){
+                IXMLElement n_xml=s_xml.getChildAtIndex(i2);
+                int x,y,z;
+                x=Integer.parseInt(n_xml.getAttribute("x","0"));
+                y=Integer.parseInt(n_xml.getAttribute("y","0"));
+                z=Integer.parseInt(n_xml.getAttribute("z","0"));
+                
+                temp_s.add_node(x,y);
+            }
+            
+            //if they are valid add them to the set list
+            proj_class.addSet(temp_s);
+            
+            //if they are not valid do not add them
+        }
+        return true;
     }
-    private void get_stage() {
-        
+    private boolean get_stage(IXMLElement xml) {
+        int num=xml.getChildrenCount();
+        if(num<1){return false;}
         //create a temporary stage object
+        IXMLElement s_xml=xml.getChildAtIndex(0);
+        stage temp_s=new stage();
+        //attributes for stage name, id, house_id, x, y, z
+        temp_s.worldx=Integer.parseInt(s_xml.getAttribute("x","0"));
+        temp_s.worldy=Integer.parseInt(s_xml.getAttribute("y","0"));
+        temp_s.setheight(Integer.parseInt(s_xml.getAttribute("z","0")));
         
+        //nodes
+        int i2;
+        int num2=s_xml.getChildrenCount();
+        for(i2=0;i2<num2;i2++){
+            IXMLElement n_xml=s_xml.getChildAtIndex(i2);
+            int x,y,z;
+            x=Integer.parseInt(n_xml.getAttribute("x","0"));
+            y=Integer.parseInt(n_xml.getAttribute("y","0"));
+            z=Integer.parseInt(n_xml.getAttribute("z","0"));
+            
+            temp_s.add_node(x,y);
+        }
         //check if the stage object is valid
-        
+        proj_class.addStage(temp_s);
         //if it is valid then load it into
-        
+        return true;
     }
-    private void get_inventory() {
-        
+    private boolean get_inventory(IXMLElement xml) {
+        int num=xml.getChildrenCount();
         
         //create a temp inventory list
-        
+        int i;
+        for(i=0;i<num;i++){
+            IXMLElement i_xml=xml.getChildAtIndex(i);
+            int id=Integer.parseInt(i_xml.getAttribute("id","0"));
+            
+            String type=i_xml.getAttribute("type","none");
+            
+            String desc=i_xml.getAttribute("desc","none");
+            
+            if(proj_class.getInstrumentByID(id)==-1){
+                //add it
+                proj_class.AddInventory(type, id, desc);
+            }
+        }
         //compare inventory list to the current one and removce any duplicate obejcts
         
         //then add the new items to the inventory
-        
+        return true;
     }
-    private void get_knowntypes() {
-        
+    private boolean get_knowntypes(IXMLElement xml) {
+        int num=xml.getChildrenCount();
         
         //create a temp list of types
-        
+        int i;
+        for(i=0;i<num;i++){
+            knowntype tempk=new knowntype();
+            IXMLElement t_xml=xml.getChildAtIndex(i);
+            String name=t_xml.getAttribute("name","default");
+            int height=Integer.parseInt(t_xml.getAttribute("height","0"));
+            String desc=t_xml.getAttribute("desc","default");
+            boolean aim=Boolean.parseBoolean(t_xml.getAttribute("aim","true"));
+            
+            tempk.setAim(aim);
+            tempk.setDesc(desc);
+            tempk.setHeight(height);
+            tempk.setName(name);
+            
+            
+            //get nodes
+            int i2;
+        int num2=t_xml.getChildrenCount();
+        for(i2=0;i2<num2;i2++){
+            IXMLElement n_xml=t_xml.getChildAtIndex(i2);
+            int x,y,z;
+            x=Integer.parseInt(n_xml.getAttribute("x","0"));
+            y=Integer.parseInt(n_xml.getAttribute("y","0"));
+            z=Integer.parseInt(n_xml.getAttribute("z","0"));
+            
+            tempk.add_node(x,y);
+        }
+            proj_class.AddType(tempk);
+        }
         //check if any types are duplicated
         
         //remove any duplicated types
         
         //add teh types to the known list
-        
+        return true;
     }
-    private void get_instrument() {
-        
+    private boolean get_instrument(IXMLElement xml) {
+         int num=xml.getChildrenCount();
         //create a temp list of instruments
-        
+        int i;
+        for(i=0;i<num;i++){
+            instrument tempi=new instrument();
+            IXMLElement i_xml=xml.getChildAtIndex(i);
+            
+            
+            tempi.setName(i_xml.getAttribute("name","default"));
+            tempi.setDescription(i_xml.getAttribute("description","default"));
+            tempi.setBarID(Integer.parseInt(i_xml.getAttribute("bar_id","0")));
+            tempi.setType(i_xml.getAttribute("type","default"));
+            tempi.worldx=Integer.parseInt(i_xml.getAttribute("x","0"));
+            tempi.worldy=Integer.parseInt(i_xml.getAttribute("y","0"));
+            tempi.worldz=Integer.parseInt(i_xml.getAttribute("z","0"));
+            tempi.setInvnetoryID(Integer.parseInt(i_xml.getAttribute("inventory_id","0")));
+            tempi.setDimmerID(Integer.parseInt(i_xml.getAttribute("dimmer_id","0")));
+            tempi.aimx=Integer.parseInt(i_xml.getAttribute("aimX","-1"));
+            tempi.aimy=Integer.parseInt(i_xml.getAttribute("aimY","-1"));
+            
+            
         //check to see if the bars exists for teh bars to go on and if they are in the right positions
         //check to see if the instruments are in inventory
-        
+            int i2;
+            int num2=i_xml.getChildrenCount();
+            for(i2=0;i2<num2;i2++){
+                IXMLElement n_xml=i_xml.getChildAtIndex(i2);
+                int x,y,z;
+                x=Integer.parseInt(n_xml.getAttribute("x","0"));
+                y=Integer.parseInt(n_xml.getAttribute("y","0"));
+                z=Integer.parseInt(n_xml.getAttribute("z","0"));
+                
+                tempi.add_node(x,y);
+            }
         //if the lisntruemnts are valid add them
-        
+        proj_class.addInstrument(tempi);
         //if the are not valid then remvoe tem
-        
+        }
+        return true;
     }
     
-    
+    private boolean get_project(IXMLElement xml,String path){
+        //reset the project to a create a new slate
+        IXMLParser parser=null;
+        IXMLReader reader=null;
+        IXMLElement housexml=null;
+        IXMLElement stagexml=null;
+        IXMLElement setxml=null;
+        IXMLElement barxml=null;
+        IXMLElement instxml=null;
+        IXMLElement invxml=null;
+        IXMLElement typexml=null;
+        
+        
+        //if(xml==null){
+        //    return false;
+        //}
+        
+        
+        proj_class.resetproject();
+        
+        String pname=xml.getAttribute("project_name","default");
+        boolean house=Boolean.valueOf(xml.getAttribute("isHouse","false"));
+        boolean stage=Boolean.valueOf(xml.getAttribute("isStage","false"));
+        boolean set=Boolean.valueOf(xml.getAttribute("isSets","false"));
+        boolean bar=Boolean.valueOf(xml.getAttribute("isBars","false"));
+        boolean ins=Boolean.valueOf(xml.getAttribute("isInstruments","false"));
+        boolean inv=Boolean.valueOf(xml.getAttribute("isInventory","false"));
+        boolean type=Boolean.valueOf(xml.getAttribute("isTypes","false"));
+        
+        //if there is a house open it
+        if(house){
+            try{
+                parser = XMLParserFactory.createDefaultXMLParser();
+                reader = StdXMLReader.fileReader(path+"/house.xml");
+                parser.setReader(reader);
+                housexml = (IXMLElement) parser.parse();
+            }catch(Exception e){
+                System.out.println("exception parsing house");
+            }
+            if(housexml!=null){
+                get_house(housexml);
+            }
+            
+            //is there a stage
+            if(stage){
+                try{
+                    parser = XMLParserFactory.createDefaultXMLParser();
+                    reader = StdXMLReader.fileReader(path+"/stage.xml");
+                    parser.setReader(reader);
+                    stagexml = (IXMLElement) parser.parse();
+                }catch(Exception e){
+                    System.out.println("exception parsing house");
+                }
+                if(stagexml!=null){
+                    get_stage(stagexml);
+                }
+            }
+            //is there a set
+            if(set){
+                try{
+                    parser = XMLParserFactory.createDefaultXMLParser();
+                    reader = StdXMLReader.fileReader(path+"/sets.xml");
+                    parser.setReader(reader);
+                    setxml = (IXMLElement) parser.parse();
+                }catch(Exception e){
+                    System.out.println("exception parsing set");
+                }
+                if(stagexml!=null){
+                    get_set(setxml);
+                }
+            }
+            //is there bars
+            if(bar){
+                try{
+                    parser = XMLParserFactory.createDefaultXMLParser();
+                    reader = StdXMLReader.fileReader(path+"/bars.xml");
+                    parser.setReader(reader);
+                    setxml = (IXMLElement) parser.parse();
+                }catch(Exception e){
+                    System.out.println("exception parsing bars");
+                }
+                if(stagexml!=null){
+                    get_bars(setxml);
+                }
+            }
+            //is there types
+            if(type){
+                try{
+                    parser = XMLParserFactory.createDefaultXMLParser();
+                    reader = StdXMLReader.fileReader(path+"/known_types.xml");
+                    parser.setReader(reader);
+                    typexml = (IXMLElement) parser.parse();
+                }catch(Exception e){
+                    System.out.println("exception parsing types");
+                }
+                if(typexml!=null){
+                    get_knowntypes(typexml);
+                }
+            }
+            //is there inventory
+            if(inv){
+                try{
+                    parser = XMLParserFactory.createDefaultXMLParser();
+                    reader = StdXMLReader.fileReader(path+"/inventory.xml");
+                    parser.setReader(reader);
+                    invxml = (IXMLElement) parser.parse();
+                }catch(Exception e){
+                    System.out.println("exception parsing inventory");
+                }
+                if(invxml!=null){
+                    get_inventory(invxml);
+                }
+            }
+            //is there a instrument
+            if(ins){
+                try{
+                    parser = XMLParserFactory.createDefaultXMLParser();
+                    reader = StdXMLReader.fileReader(path+"/instruments.xml");
+                    parser.setReader(reader);
+                    instxml = (IXMLElement) parser.parse();
+                }catch(Exception e){
+                    System.out.println("exception parsing instrument");
+                }
+                if(instxml!=null){
+                    get_instrument(instxml);
+                }
+            }
+            
+            
+        }
+        
+        
+        return true;
+    }
 }
