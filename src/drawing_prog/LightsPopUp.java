ad24fbd524a2e70ea8e78af6481623482caa7114
joshzaw <joshzaw>
2005-04-21 06:23:49 +0000

    Added to mkae a way to assign lights from inventory to the plot

---
+++ src/drawing_prog/LightsPopUp.java
@@ -0,0 +1,84 @@
+/*
+ * LightsPopUp.java
+ *
+ * Created on April 20, 2005, 12:23 AM
+ */
+
+package drawing_prog;
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import Data_Storage.*;
+import theatre.*;
+
+/**
+ *
+ * @author root
+ */
+public class LightsPopUp extends JDialog implements ActionListener{
+    protected project proj_class;
+    protected int iScreenHeight =BasicWindow.iScreenWidth-50;
+    protected int iScreenWidth = BasicWindow.iScreenHeight-100;
+    //components
+    private JComboBox jcInst = new JComboBox();
+    
+    /** Creates a new instance of LightsPopUp */
+    public LightsPopUp() {
+        super(BasicWindow.curWindow,"Instrument Selector", true);
+        //setTitle("Inventory Manager");
+        proj_class=(project)project.oClass;
+        
+        addComponents();
+        setBounds(10,50, iScreenHeight,iScreenWidth);
+        //setSize(500,500);
+        setResizable(true);
+        setVisible(true);
+    }
+    
+    public void addComponents() {
+        JPanel jpMain = new JPanel();
+        jpMain.setLayout(null);
+        
+        loadItems();
+        jcInst.setBounds(250, 15, 150, 20 );
+        jpMain.add( jcInst );
+        
+        
+        JButton jbOk = new JButton("Ok");
+        jbOk.addActionListener(this);
+        jbOk.setActionCommand("ok");
+        jbOk.setBounds(250, 70, 150, 20);
+        jpMain.add(jbOk);
+        
+        JButton jbCancel = new JButton("Cancel");
+        jbCancel.addActionListener(this);
+        jbCancel.setActionCommand("cancel");
+        jbCancel.setBounds(375, 70, 250, 20);
+        jpMain.add(jbCancel);
+        
+        getContentPane().add(jpMain);
+    }
+    
+    public void loadItems() {
+        jcInst.removeAllItems();
+        int i;
+        for(i=0;i<proj_class.inventories.getNumItems();i++){
+            if(!proj_class.inventories.getItemUsed(i)){ 
+                jcInst.addItem(String.valueOf(proj_class.inventories.getItemID(i)));
+            }
+        }
+        jcInst.validate();
+    }
+    
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+        if (e.getActionCommand().equals("ok")) {
+            proj_class.templightid=Integer.parseInt((String)jcInst.getSelectedItem());
+            this.dispose();
+        }else if (e.getActionCommand().equals("cancel")) {
+            proj_class.templightid=-1;
+            this.dispose();
+        }
+    }
+}
