4bf93ac1b777f6531832834e28ef9e208f4adf25
joshzaw <joshzaw>
2005-06-26 00:47:30 +0000

    got the sorting of lights to work

--- src/theatre/WiringPlot.java
+++ src/theatre/WiringPlot.java
@@ -6,8 +6,8 @@
 
 package theatre;
 import Data_Storage.*;
-import java.awt.*; 
-import java.awt.event.*; 
+import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 import java.io.*;
@@ -85,17 +85,29 @@ public class WiringPlot extends JDialog implements ActionListener{
         getContentPane().add(jp);
     }
     private void makeTable() {
-        Object[] columnNames =  {   "Bar",
-                                    "Dimmer",
-                                    "Light/Item"
-                                };
-
-        Object[][]  data    =  populateTable();
-        outTable = new javax.swing.JTable(data, columnNames);
+        String[] columnNames =  {   "Bar",
+                "Dimmer",
+                "Light/Item"
+        };
         
+        Object[][]  data    = populateTable();
+                
+                /*{
+            {   "Bar1", "Dimmer","Light/Item"
+            },
+            {   "Bar2","Dimmer","Light/Item"
+            },
+            {   "Bar3","Dimmer","Light/Item"
+            }};
+        */
+        
+        //populateTable();
+        outTable = new javax.swing.JTable(data, columnNames);
+       
         outTable.setBounds(30,30,500,465);
         outTable.setVisible(true);
         jp.add(outTable);
+        jp.validate();
     }
     private Object[][] populateTable() {
         int x = 1;
@@ -103,25 +115,78 @@ public class WiringPlot extends JDialog implements ActionListener{
         Object[][] retval;
         
         //TEMP
-        String[] a = new String[5];
-        a = new String[] {"bar", "a", "b","c","d"};
+        String[] a = new String[3];
+        a = new String[] {"bar", "a", "b"};
         
-        retval = new Object[][] {
-                    {a,a,a}
-        };
+        retval = new Object[proj_class.instruments.get_num_objects()+1][3];
+        
         if(selection == 1){
             //handle sort on dimmer
-            
-        }
-        else if(selection == 2){
+            System.out.println("sorting by dimmer");
+        } else if(selection == 2){
             //handle sort on light
+            System.out.println("sorting by light");
+            retval[0][0]=new String("Instrument");
+            retval[0][1]=new String("Dimmer");
+            retval[0][2]=new String("Bar");
             
-        }
-        else if(selection == 3){
+            if(proj_class.instruments.get_num_objects()>0){
+                //there are lights that we can display
+                //get the lowest value instrument
+                
+                instrument last = getNextInstrument(null);
+                
+                
+                
+                int i=1;
+                while(last!=null){
+                    //add last to the list to display
+                    retval[i][0]=new Integer(last.getInventoryID());
+                    retval[i][1]=new Integer(last.Associated_dimmerID);
+                    retval[i][2]=new Integer(last.Associated_barID);
+                    i++;
+                    last = getNextInstrument(last);
+                }
+                
+                
+            }
+            
+            
+            
+            
+            
+        } else if(selection == 3){
             //Handle Sort on Bar
             
-        }
-        else {
+            System.out.println("sorting by bar");
+            retval[0][0]=new String("Bar");
+            retval[0][1]=new String("Instrument");
+            retval[0][2]=new String("Dimmer");
+            
+            if(proj_class.instruments.get_num_objects()>0){
+                //there are lights that we can display
+                //get the lowest value instrument
+                
+                instrument last = getNextInstrument(null);
+                
+                
+                
+                int i=1;
+                while(last!=null){
+                    //add last to the list to display
+                    retval[i][0]=new Integer(last.getInventoryID());
+                    retval[i][1]=new Integer(last.Associated_dimmerID);
+                    retval[i][2]=new Integer(last.Associated_barID);
+                    i++;
+                    last = getNextInstrument(last);
+                }
+                
+                
+            }
+            
+            
+            
+        } else {
             System.out.println("Error In UI Wiring Handled");
             //This section handles extra input situation
         }
@@ -131,18 +196,54 @@ public class WiringPlot extends JDialog implements ActionListener{
         if(e.getActionCommand() == "bar"){
             selection = 3;
             makeTable();
-        }
-        else if(e.getActionCommand() == "dimmer"){
+        } else if(e.getActionCommand() == "dimmer"){
             selection = 1;
             makeTable();
-        }
-        else if(e.getActionCommand() == "light"){
+        } else if(e.getActionCommand() == "light"){
             selection = 2;
             makeTable();
-        }
-        else if(e.getActionCommand() == "print"){
+        } else if(e.getActionCommand() == "print"){
             PrintUtilities.printComponent(outTable);
         }
         
     }
+    
+    
+    //get functions to retrieve the next item that should be displayed
+    //this method is not the optimal solution but it should work fine
+    //will have to test performance after it is done
+    
+    public instrument getNextInstrument(instrument last){
+        instrument temp=null;
+        
+        for(int i=0;i<proj_class.instruments.get_num_objects();i++){
+            //base case
+            if(last!=null){
+                if(((instrument)proj_class.instruments.get_object(i)).InventoryID>last.InventoryID){
+                    if(temp==null){
+                        temp=(instrument)proj_class.instruments.get_object(i);
+                    } else if(((instrument)proj_class.instruments.get_object(i)).InventoryID<temp.InventoryID){
+                        temp=(instrument)proj_class.instruments.get_object(i);
+                    }
+                }
+            }else{
+                if(temp==null){
+                    temp=(instrument)proj_class.instruments.get_object(i);
+                } else if(((instrument)proj_class.instruments.get_object(i)).InventoryID<temp.InventoryID){
+                    temp=(instrument)proj_class.instruments.get_object(i);
+                }
+            }
+        }
+        
+        return temp;
+    }
+    
+    public instrument getNextInstrumentOnBar(instrument last,bar theBar){
+        instrument temp=null;
+        
+        return temp;
+    }
+    
+    
+    
 }
